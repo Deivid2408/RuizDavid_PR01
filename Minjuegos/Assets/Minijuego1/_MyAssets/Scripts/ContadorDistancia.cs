16fed8045951e8373e363ea674c2aa7d009a8900
Deivid2408 <davidruizbermejo99@gmail.com>
2025-11-17 19:03:09 +0100

    Movimiento plataformas minijuego 2
    He conseguido solucionar el movimiento de las plataformas con el joystick derecho. Faltaba indicar correctamente la dirección.

--- Minjuegos/Assets/Minijuego1/_MyAssets/Scripts/ContadorDistancia.cs
+++ Minjuegos/Assets/Minijuego1/_MyAssets/Scripts/ContadorDistancia.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using TMPro;
 
-public class Meta : MonoBehaviour
+public class ContadorDistancia : MonoBehaviour
 {
+    public float contador;
+    public float distancia;
 
+    public TMP_Text textocontador;
+    public TMP_Text textodistancia;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      
     }
 
     // Update is called once per frame
@@ -15,6 +19,4 @@ public class Meta : MonoBehaviour
     {
         
     }
-
-   
 }
--- Minjuegos/Assets/Minijuego2/_MyAssets/Scripts/MovPlataformaFila2y4.cs
+++ Minjuegos/Assets/Minijuego2/_MyAssets/Scripts/MovPlataformaFila2y4.cs
@@ -24,8 +24,9 @@ public class MovPlataformaFila2y4 : MonoBehaviour
 
     void Rotacion()
     {
+        
         //Esto será mi movimiento de mi joystick 
-        float rotacion = Input.GetAxis("Horizontal2");
-        rb.AddTorque(Vector3.right * rotacion * fuerzaRotacion);
+        float rotacion = Input.GetAxis("Horizontal3");
+        rb.AddTorque(Vector3.forward * rotacion * fuerzaRotacion);
     }
 }
